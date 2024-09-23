1712b6e3ae3b65402f36d7a093d2baa4e4e8f649
masuim <po.n31227@gmail.com>
2024-09-23 16:00:34 +0900

    fix: SaurusImageのスタイル修正

--- src/components/saurus-image.tsx
+++ src/components/saurus-image.tsx
@@ -8,7 +8,13 @@ export const SaurusImage = ({ saurusType }: Props) => {
   const imagePath = `/images/saurus/${saurusType}/${saurusType}1.png`;
   return (
     <div className="mb-4">
-      <img src={imagePath} alt={saurusType} width={200} height={200} />
+      <img
+        src={imagePath}
+        alt={saurusType}
+        width={200}
+        height={200}
+        className="rounded-lg shadow-lg"
+      />
     </div>
   );
 };
