53137ecdb39435af1a95c987b3d472170dcd2ca2
masuim <po.n31227@gmail.com>
2024-09-24 09:00:55 +0900

    WIP |  mainCOntentsのレイアウト調整

--- src/components/main-contents.tsx
+++ src/components/main-contents.tsx
@@ -44,28 +44,24 @@ export const MainContents = ({
   };
 
   return (
-    <main className="xs:p-10 flex-1 items-center bg-yellow-200 p-10 md:p-20 lg:p-32">
+    <main className="flex-1 items-center bg-yellow-200 p-10">
       <div className="xs:flex-row xs:flex-wrap xs:justify-center flex flex-col items-center bg-orange-300">
-        <div className="xs:flex-row xs:flex-wrap xs:justify-center flex w-full flex-col items-center bg-purple-200">
-          <div className="xs:w-full xs:flex-row xs:flex-wrap xs:justify-center flex flex-col items-center bg-red-200">
-            <SaurusImage
-              saurusType={activeNicca?.saurusType ?? 'brachiosaurus'}
-              className="xs:w-1/2"
-            />
-            <div className="xs:mt-0 xs:w-1/2 xs:flex-row xs:flex-wrap xs:justify-center flex flex-col items-center bg-red-200">
-              <div className="flex w-full bg-blue-200">
-                {/* TODO: それぞれのボタンのmax-widthを設定する */}
-                <CompleteButton className="xs:w-3/5" />
-                <NiccaEditButton className="xs:w-1/5" />
-                <NiccaDeleteButton className="xs:w-1/5" />
-              </div>
-              <NiccaMessage className="xs:mt-4 mt-4" />
+        <div className="xs:w-full xs:flex-row xs:flex-wrap xs:justify-center flex flex-col items-center bg-red-200">
+          <SaurusImage
+            saurusType={activeNicca?.saurusType ?? 'brachiosaurus'}
+            className="xs:w-1/2"
+          />
+          <div className="xs:mt-0 xs:w-1/2 xs:flex-row xs:flex-wrap xs:justify-center flex flex-col items-center bg-gray-200">
+            <div className="flex w-full bg-blue-200">
+              {/* TODO: それぞれのボタンのmax-widthを設定する */}
+              <CompleteButton className="xs:w-3/5" />
+              <NiccaEditButton className="xs:w-1/5" />
+              <NiccaDeleteButton className="xs:w-1/5" />
             </div>
-          </div>
-          <div className="xs:mt-4 w-full">
-            <CustomCalendar className="xs:w-full" />
+            <NiccaMessage className="xs:mt-4 mt-4" />
           </div>
         </div>
+        <CustomCalendar className="xs: mt-4 w-full bg-green-200" />
       </div>
       {isRegisterModalOpen && (
         <NiccaRegisterModal
